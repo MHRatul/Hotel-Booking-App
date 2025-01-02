01b64296d52c85a5b73d6d0415205704b91d5232
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2025-01-02 12:49:07 +0600

    shadcn check- Is it working or not ?

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div>Home page <Button variant="secondary">Test</Button></div>
   );
 }
